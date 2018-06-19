40350f2a2beca9f761d64b256ca8923982a55510
Insomnia <282132315@qq.com>
2018-06-19 08:49:30 +0800

    sync

---
+++ CPP/ListNode/142/LinkedListCycle2.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Insomnia on 2018/6/15.
+//
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+
+int main() {
+
+    ListNode a1(1);
+    ListNode a2(2);
+    ListNode a3(3);
+    ListNode a4(4);
+    ListNode a5(5);
+    ListNode a6(6);
+    ListNode a7(7);
+
+    a1.next = &a2;
+    a2.next = &a3;
+    a3.next = &a4;
+    a4.next = &a5;
+    a5.next = &a6;
+    a6.next = &a7;
+    a7.next = &a3;
+
+//    Solution solution;
+//    ListNode *head = solution.detectCycle(&a1);
+//    std::cout << head->val << std::endl;
+    return 0;
+}
---
+++ CPP/ListNode/160/IntersectionTwoLinkedLists.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Insomnia on 2018/6/14.
+//
+
+#include <stdio.h>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+
+    }
+};
+
+int main() {
+
+    return 0;
+}
