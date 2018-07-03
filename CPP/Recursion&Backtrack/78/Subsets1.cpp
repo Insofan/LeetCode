36f0b58097545fb62a5a1de2162bc1071ca68dd3
Insomnia <282132315@qq.com>
2018-07-03 21:49:38 +0800

    78 subsets 位运算版

--- CPP/Recursion&Backtrack/78/Subsets1.cpp
+++ CPP/Recursion&Backtrack/78/Subsets1.cpp
@@ -4,10 +4,25 @@
 
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
 class Solution {
 public:
+    vector<vector<int>> subsets(vector<int> &nums) {
+        vector<vector<int> > result;
+        int allSet = 1 << nums.size();
+        for (int i = 0; i < allSet; ++i) {
+           vector<int> item;
+            for (int j = 0; j < nums.size(); ++j) {
+               if (i & (1 << j)) {
+                   item.push_back(nums[j]);
+               }
+            }
+            result.push_back(item);
+        }
+        return result;
+    }
 
 };
 
@@ -18,9 +33,9 @@ int main() {
     nums.push_back(2);
     nums.push_back(3);
 
-    vector<vector<int>>res;
+    vector<vector<int>> res;
     Solution sol;
-    res =  sol.subsets(nums);
+    res = sol.subsets(nums);
 
     for (int i = 0; i < res.size(); ++i) {
         for (int j = 0; j < res[i].size(); ++j) {
