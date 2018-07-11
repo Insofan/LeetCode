2ac01b0c472d5684fa3a91ab2adfc6a8be2d8a3f
Insomnia <282132315@qq.com>
2018-07-12 00:04:56 +0800

    236 最小公共祖先

--- CPP/TreeNode/236/LowestCommonAncestorOfABinaryTree.cpp
+++ CPP/TreeNode/236/LowestCommonAncestorOfABinaryTree.cpp
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <vector>
+#include <iostream>
 
 struct TreeNode {
     int val;
@@ -13,10 +14,34 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+
+using namespace std;
 class Solution {
 public:
+    //用两个栈存储遍历的节点
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+        vector<TreeNode *> path;
+        vector<TreeNode *> nodePPath;
+        vector<TreeNode *> nodeQPath;
+        int finish = 0;
+        preorder(root, p, path, nodePPath, finish);
+        path.clear();
+        finish = 0;
+        preorder(root, q, path, nodeQPath, finish);
+        int pathLen = 0;
+        if (nodePPath.size() < nodeQPath.size()) {
+            pathLen = nodePPath.size();
+        } else {
+            pathLen = nodeQPath.size();
+        }
 
+        TreeNode *result = 0;
+        for (int i = 0; i < pathLen; ++i) {
+            if (nodePPath[i] == nodeQPath[i]) {
+                result = nodePPath[i];
+            }
+        }
+        return result;
     }
 
     void preorder(TreeNode *node, TreeNode *search, std::vector<TreeNode *> &path, std::vector<TreeNode *> &result, int &finish) {
@@ -38,6 +63,32 @@ public:
 };
 
 int main() {
+    TreeNode a(3);
+    TreeNode b(5);
+    TreeNode c(1);
+    TreeNode d(6);
+    TreeNode e(2);
+    TreeNode f(0);
+    TreeNode x(8);
+    TreeNode y(7);
+    TreeNode z(4);
+
+    a.left = &b;
+    a.right = &c;
+    b.left = &d;
+    b.right = &e;
+    c.left = &f;
+    c.right = &x;
+    e.left = &y;
+    e.right = &z;
+
+    Solution sol;
+    TreeNode *result = sol.lowestCommonAncestor(&a, &b, &f);
+    cout << result->val << endl;
+    result = sol.lowestCommonAncestor(&a, &d, &z);
+    cout << result->val << endl;
+    result = sol.lowestCommonAncestor(&a, &b, &y);
+    cout << result->val << endl;
 
     return 0;
 }
