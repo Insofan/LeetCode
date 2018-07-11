5e93366ef8cf887ddf3aaaf876e71d7f8b9af927
Insomnia <282132315@qq.com>
2018-07-12 00:28:33 +0800

    114 vector 做法

---
+++ CPP/TreeNode/114/FlattenBinaryTreetoLinkedListI.cpp
@@ -0,0 +1,45 @@
+// 
+// Created by Insomnia on 2018/7/12.
+//
+
+//用vector 完成， 不够优秀 但能完成
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    void flatten(TreeNode *root) {
+        vector<TreeNode *> nodeVec;
+        preOrder(root, nodeVec);
+        for (int i = 1; i < nodeVec.size(); ++i) {
+            root->left= NULL;
+            root->right = nodeVec[i];
+        }
+
+    }
+
+private:
+    void preOrder(TreeNode *node, vector<TreeNode *> &nodeVec) {
+        if (!node) {
+            return;
+        }
+
+        nodeVec.push_back(node);
+        preOrder(node->left, nodeVec);
+        preOrder(node->right, nodeVec);
+    }
+};
+
+int main() {
+
+}
