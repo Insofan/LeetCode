ae86fe7cd857516bb75fbe6af7c5468fe30c21e6
Insofan <282132315@qq.com>
2018-07-31 16:19:57 +0800

    sync

---
+++ CPP/Search/473/MatchsticksToSquare.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Insomnia on 2018/7/31.
+//
+
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    bool makesquare(vector<int>& nums) {
+
+    }
+};
+
+int main() {
+
+    return 0;
+}
