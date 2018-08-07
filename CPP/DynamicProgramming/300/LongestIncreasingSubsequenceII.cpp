193925fe7528355ca81d34507c8f3f8bb2525fa7
Insofan <282132315@qq.com>
2018-08-07 11:01:56 +0800

    300

---
+++ CPP/DynamicProgramming/300/LongestIncreasingSubsequenceII.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Insomnia on 2018/8/7.
+//
+
+#include <stdio.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int> &nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+
+
+        vector<int> stack;
+
+        stack.push_back(nums[0]);
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > stack.back()) {
+                stack.push_back(nums[i]);
+            } else {
+                for (int j = 0; j < stack.size(); ++j) {
+                    if (stack[j] >= nums[i]) {
+                        stack[j] = nums[i];
+                        break;
+                    }
+                }
+            }
+        }
+        return stack.size();
+    }
+};
+
+int main() {
+
+    return 0;
+}
