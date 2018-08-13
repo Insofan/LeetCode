2899cc4285348729a360bc748669b10c76c7efc9
Insofan <282132315@qq.com>
2018-08-13 09:53:21 +0800

    10 frog jump offer

--- CPP/Offer/10/JumpFrog.cpp
+++ CPP/Offer/10/JumpFrog.cpp
@@ -2,13 +2,40 @@
 // Created by Insomnia on 2018/8/13.
 //
 
+//同 leetcode 70
+
 #include <stdio.h>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+class Solution {
+public:
+    int jumpFrog(int n) {
+        vector<int> dp(n, -1);
+
+        dp[0] = 1;
+        dp[1] = 2;
+
+        if (n < 2) {
+            return dp[n - 1];
+        }
+
+        for (int i = 2; i < n; ++i) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        return dp[n - 1];
+    }
+};
+
 int main() {
+
+    cout << "Frog jump 10 ladder:" << endl;
+    Solution solution;
+
+    cout << solution.jumpFrog(3) << endl;
 
     return 0;
 }
