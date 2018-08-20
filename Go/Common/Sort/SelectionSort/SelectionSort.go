9392b17e5e024ad0c97e026096c573657490c190
Insofan <282132315@qq.com>
2018-08-20 10:46:35 +0800

    go SelectionSort

---
+++ Go/Common/Sort/SelectionSort/SelectionSort.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+	"fmt"
+)
+
+func randomArr(len, maxNum int) []int {
+	tempArr := []int{}
+	rand.Seed(time.Now().Unix())
+
+	for i := 0; i < len; i++ {
+		x := rand.Intn(maxNum)
+		tempArr = append(tempArr, x)
+	}
+	return tempArr
+}
+
+func selectionSort(arr []int) {
+	for i, _ := range arr {
+		key := i
+		for j := i + 1; j < len(arr); j++ {
+			if (arr[j] < arr[key]) {
+				key = j
+			}
+		}
+		arr[i], arr[key] = arr[key], arr[i]
+	}
+}
+
+func main() {
+	arr := randomArr(10, 50)
+	for _, val := range arr {
+		fmt.Printf("%d ", val)
+	}
+	fmt.Println()
+	selectionSort(arr)
+	for _, val := range arr {
+		fmt.Printf("%d ", val)
+	}
+}
