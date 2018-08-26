69ab04a9434be5d4b67c472bf5399e729f1e7ec9
Insomnia <282132315@qq.com>
2018-08-26 23:46:52 +0800

    23 offer

---
+++ CPP/Offer/23/RecycleLinkListOffer.cpp
@@ -0,0 +1,73 @@
+// 
+// Created by Insomnia on 18-8-26.
+//
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x): val(x), next(NULL){}
+};
+
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *meet = NULL;
+
+        while (fast) {
+            slow = slow->next;
+            fast = fast->next;
+            if (!fast) {
+                return NULL;
+            }
+            fast = fast->next;
+
+            if (fast == slow) {
+                meet = fast;
+                break;
+            }
+        }
+
+        if (!meet) {
+            return  NULL;
+        }
+
+        while (meet && head) {
+            if (meet == head) {
+                return head;
+            }
+            meet = meet->next;
+            head = head->next;
+        }
+        return NULL;
+    }
+};
+int main() {
+    ListNode a1(1);
+    ListNode a2(2);
+    ListNode a3(3);
+    ListNode a4(4);
+    ListNode a5(5);
+    ListNode a6(6);
+    ListNode a7(7);
+
+    a1.next = &a2;
+    a2.next = &a3;
+    a3.next = &a4;
+    a4.next = &a5;
+    a5.next = &a6;
+    a6.next = &a7;
+    a7.next = &a3;
+
+    Solution solution;
+    ListNode *head = solution.detectCycle(&a1);
+    std::cout << head->val << std::endl;
+    return  0;
+}
