50e5e16a273d6164112fd899e1b105b7e95beb85
Insofan <282132315@qq.com>
2018-08-27 08:41:27 +0800

    leet code 136

--- CPP/Bit/136/SingleNumberII.cpp
+++ CPP/Bit/136/SingleNumberII.cpp
@@ -3,9 +3,36 @@
 //
 
 #include <stdio.h>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+
+        int res = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+           res = res ^ nums[i];
+           cout << res << endl;
+        }
+
+        return res;
+    }
+};
 
 
 int main() {
-    
+    int         test1[7] = {4, 1, 2, 1, 2, 9, 4};
+    vector<int> vec;
+    for (int    i        = 0; i < 7; ++i) {
+        vec.push_back(test1[i]);
+    }
+
+    Solution solution;
+    cout << solution.singleNumber(vec) << endl;
     return 0;
 }
