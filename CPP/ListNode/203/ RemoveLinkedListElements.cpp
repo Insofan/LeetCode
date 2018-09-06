74d976b6c3951fda861392b943ccfd8ce97f4a61
Insofan <282132315@qq.com>
2018-09-06 11:02:42 +0800

    leetcode 203 83

--- CPP/ListNode/203/ RemoveLinkedListElements.cpp
+++ CPP/ListNode/203/ RemoveLinkedListElements.cpp
@@ -3,7 +3,8 @@
 //
 
 #include <stdio.h>
-
+#include <iostream>
+using namespace std;
 
 struct ListNode {
     int      val;
@@ -15,11 +16,56 @@ struct ListNode {
 class Solution {
 public:
     ListNode *removeElements(ListNode *head, int val) {
+        ListNode preHead = ListNode(INT32_MIN);
+        preHead.next = head;
+        ListNode *ptr = &preHead;
+        while (ptr) {
+            if (ptr->next && (ptr->next->val == val)) {
+                ptr->next = ptr->next->next;
+            } else {
+                ptr = ptr->next;
+            }
+        }
 
+        return preHead.next;
     }
 };
 
+void printListNode(ListNode *node) {
+    while (node) {
+        cout << node->val << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
 int main() {
+
+    ListNode a(1);
+    ListNode b(2);
+    ListNode c(3);
+    ListNode d(4);
+    ListNode e(5);
+    ListNode f(5);
+
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+    d.next = &e;
+    e.next = &f;
+
+    printListNode(&a);
+
+    Solution solution;
+    ListNode *head = &a;
+
+
+    ListNode *res = solution.removeElements(&a, 1);
+    printListNode(res);
+    res = solution.removeElements(res, 5);
+    printListNode(res);
+    res = solution.removeElements(res, 3);
+    printListNode(res);
 
     return 0;
 }
--- CPP/ListNode/83/RemoveDuplicatesFromSortedList.cpp
+++ CPP/ListNode/83/RemoveDuplicatesFromSortedList.cpp
@@ -3,22 +3,64 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int      val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode *ptr = head;
+        ListNode preHead = ListNode(INT32_MIN);
+        preHead.next = head;
+        ListNode *ptr = &preHead;
         while (ptr) {
-            if (ptr->next && (ptr->val == ptr->next->val)) {
+            if (ptr->next && ptr->val == ptr->next->val) {
                 ptr->next = ptr->next->next;
             } else {
                 ptr = ptr->next;
             }
         }
-        return head;
+
+        return preHead.next;
     }
 };
 
+void printListNode(ListNode *node) {
+    while (node) {
+        cout << node->val << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
 int main() {
+
+    ListNode a(1);
+    ListNode b(1);
+    ListNode c(2);
+    ListNode d(3);
+    ListNode e(3);
+    ListNode f(3);
+
+    a.next = &b;
+    b.next = &c;
+    c.next = &d;
+    d.next = &e;
+    e.next = &f;
+    ListNode *head = &a;
+    printListNode(head);
+
+    Solution solution;
+
+
+    ListNode *res = solution.deleteDuplicates(head);
+    printListNode(res);
+
 
     return 0;
 }
