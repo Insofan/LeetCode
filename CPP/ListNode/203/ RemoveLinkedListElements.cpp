3c71fb5ebda86a034d82106c4ec5470f8af83de3
Insofan <282132315@qq.com>
2018-09-06 10:44:58 +0800

    sync

---
+++ CPP/ListNode/203/ RemoveLinkedListElements.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Insomnia on 2018/9/5.
+//
+
+#include <stdio.h>
+
+
+struct ListNode {
+    int      val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *removeElements(ListNode *head, int val) {
+
+    }
+};
+
+int main() {
+
+    return 0;
+}
