42c52d87930ec0a65dea2831d05ef2c4a00b222c
Insofan <282132315@qq.com>
2018-09-29 09:58:34 +0800

    offer 41 bingo

--- CPP/Offer/41/FindMedianFromDataStreamOffer.cpp
+++ CPP/Offer/41/FindMedianFromDataStreamOffer.cpp
@@ -17,32 +17,62 @@ public:
     }
 
     void addNum(int num) {
-        fir.push(num);
-        sec.push(fir.top());
-        if (fir.size() < sec.size()) {
-            fir.push(sec.top());
+        if (largeQueue.size() == 0) {
+            largeQueue.push(num);
+        } else if (largeQueue.size() > smallQueue.size()) {
+            if (num < largeQueue.top()) {
+                int top = largeQueue.top();
+                largeQueue.pop();
+                largeQueue.push(num);
+
+                smallQueue.push(top);
+            } else {
+                smallQueue.push(num);
+            }
+        } else if (smallQueue.size() > largeQueue.size()) {
+            if (num < smallQueue.top()) {
+                largeQueue.push(num);
+            } else {
+                largeQueue.push(smallQueue.top());
+                smallQueue.pop();
+                smallQueue.push(num);
+            }
+        } else {
+            if (num < largeQueue.top()) {
+                largeQueue.push(num);
+            } else {
+                smallQueue.push(num);
+            }
         }
     }
 
     double findMedian() {
-        return fir.size() > sec.size() ? fir.top() : (fir.top() + sec.top()) / 2.0;
+        int size = smallQueue.size() +largeQueue.size();
+        if (size % 2 == 0) {
+            return  (smallQueue.top() + largeQueue.top()) / 2.0;
+        }
+        return smallQueue.size() > largeQueue.size() ? smallQueue.top():largeQueue.top();
     }
+
 private:
-    priority_queue<int, vector<int>> fir;
-    priority_queue<int, vector<int>, greater> sec;
+    //大堆
+    priority_queue<int, vector<int>, greater<int>>          smallQueue;
+    //小堆
+    priority_queue<int, vector<int>> largeQueue;
 };
 
 int main() {
     MedianFinder medianFinder;
 
-     medianFinder.addNum(1);
+    /*
+    medianFinder.addNum(1);
     medianFinder.addNum(2);
-    double  res = medianFinder.findMedian();
+    double res = medianFinder.findMedian();
     cout << res << endl;
     medianFinder.addNum(3);
     res = medianFinder.findMedian();
     cout << res << endl;
-    /*
+     */
     medianFinder.addNum(-1);
     medianFinder.addNum(-2);
     medianFinder.addNum(-3);
@@ -50,6 +80,5 @@ int main() {
     medianFinder.addNum(-5);
     double res = medianFinder.findMedian();
     cout << res << endl;
-     */
     return 0;
 }
