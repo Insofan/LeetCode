c3a7c0465c3759a175eb47981eb3e85b4fc45a1e
Insomnia <282132315@qq.com>
2019-01-22 00:20:36 +0800

    sync

--- Python3/Offer/64/1ToNSum.py
+++ Python3/Offer/64/1ToNSum.py
@@ -9,13 +9,16 @@
 # python 跑不通, 看cpp版本
 class Solution:
     def sum(self, n):
-        sum = n
-        if sum > 0:
-            sum += self.sum(n - 1)
+        res = n[0]
+        if res == 0:
+            return res
         else:
-            return sum
+            n[0] -= 1
+            res += sum(n[0])
+        return res
 if __name__ == '__main__':
     print("1 to n sum")
     print("1 to 100 sum:")
     sol = Solution()
-    print(sol.sum(100))
+    arg = [100]
+    print(sol.sum(arg))
