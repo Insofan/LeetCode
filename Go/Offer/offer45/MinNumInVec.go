31693293c98baedd0a23490698dc7329358e861e
Insofan <282132315@qq.com>
2019-01-29 10:42:07 +0800

    offer 45 bingo

--- Go/Offer/offer45/MinNumInVec.go
+++ Go/Offer/offer45/MinNumInVec.go
@@ -9,29 +9,36 @@
 package offer45
 
 import (
-	"fmt"
+	"sort"
+	"strconv"
 )
 
 // MinNumInVec ...
-func MinNumInVec(vec []int) int {
+func MinNumInVec(vec []int) string {
 	if len(vec) == 0 {
-		return 0
+		return "0"
 	}
 
-	ru := make([]rune, len(vec))
+	resSize := 0
+	bv := make(btVec, len(vec))
 	for i := 0; i < len(vec); i++ {
-		ru[i] = rune(vec[i])
+		bv[i] = []byte(strconv.Itoa(vec[i]))
+		resSize += len(bv[i])
 	}
 
-	fmt.Println(ru)
-	if ru[1]+ru[2] < ru[2]+ru[1] {
-		fmt.Println(ru[1], ru[2])
-	} else {
-		fmt.Println(ru[2], ru[1])
+	sort.Sort(bv)
+
+	res := make([]byte, 0, resSize)
+	for i := len(vec) - 1; i >= 0; i-- {
+		res = append(res, bv[i]...)
 	}
-	//sort.Sort(btVec(ru))
-	fmt.Println(ru)
-	return -1
+	i := 0
+
+	for i < resSize-1 && res[i] == '0' {
+		i++
+	}
+
+	return string(res[i:])
 }
 
 type btVec [][]byte
@@ -42,6 +49,22 @@ func (bv btVec) Len() int {
 
 func (bv btVec) Less(i, j int) bool {
 	size := len(bv[i]) + len(bv[j])
+	bij := make([]byte, 0, size)
+	bij = append(bij, bv[i]...)
+	bij = append(bij, bv[j]...)
+
+	bji := make([]byte, 0, size)
+	bji = append(bji, bv[j]...)
+	bji = append(bji, bv[i]...)
+
+	for k := 0; k < size; k++ {
+		if bij[k] < bji[k] {
+			return true
+		} else if bij[k] > bji[k] {
+			return false
+		}
+	}
+	return false
 }
 
 func (bv btVec) Swap(i, j int) {
